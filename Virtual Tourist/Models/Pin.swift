2bf46f6fa2886e4698a5e69c0bf8dc6082903e5e
akhilmantha <akhilmantha@live.com>
2018-08-23 19:58:27 +0530

    refactoring view controller extensions

--- Virtual Tourist/Models/Pin.swift
+++ Virtual Tourist/Models/Pin.swift
@@ -1,3 +1,12 @@
+//
+//  Pin.swift
+//  Virtual Tourist
+//
+//  Created by akhil mantha on 23/08/18.
+//  Copyright © 2018 akhil mantha. All rights reserved.
+//
+
+import Foundation
 import CoreData
 
 @objc(Pin)
@@ -15,12 +24,5 @@ public class Pin: NSManagedObject {
         }
     }
     
-}//
-//  Pin.swift
-//  Virtual Tourist
-//
-//  Created by akhil mantha on 23/08/18.
-//  Copyright © 2018 akhil mantha. All rights reserved.
-//
+}
 
-import Foundation
